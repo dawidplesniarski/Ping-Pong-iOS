3b361aa7ab39a5ba7c9a41247c38255cded3d595
dawidplesniarski <dawidplesniarski@gmail.com>
2020-01-08 11:49:48 +0100

    Scene, paddles and ball added

--- Game/GameScene.swift
+++ Game/GameScene.swift
@@ -10,9 +10,23 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    
+    var ball = SKSpriteNode()
+    var main = SKSpriteNode()
+    var enemy = SKSpriteNode()
+    
 
     override func didMove(to view: SKView) {
-    
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        main = self.childNode(withName: "main") as! SKSpriteNode
+        enemy = self.childNode(withName: "enemy") as! SKSpriteNode
+        
+        ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        border.friction = 0
+        border.restitution = 1
+        
+        self.physicsBody = border
     }
     
     override func update(_ currentTime: TimeInterval) {
